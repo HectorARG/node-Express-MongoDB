c8d8681dcbc74a0bf2825cdfc2c576acdc9a7c6e
hector.romang <hector.romang@durango.gob.mx>
2022-12-15 14:52:27 -0600

    Validaciones en campos obligatorio registro

--- routes/usuarios.js
+++ routes/usuarios.js
@@ -2,12 +2,22 @@
     Ruta: /api/usuarios
 */
 const { Router } = require('express');
+const { check } = require('express-validator');
 const { getUsuarios, postUsuarios } =  require('../controllers/usuarios');
+const { validarCampos } = require('../middlewares/validar-campos');
 
 const router = Router();
 
 router.get('/', getUsuarios );
-router.post('/', postUsuarios );
+router.post(
+    '/', 
+    [
+        check('nombre', 'El nombre es obligatorio').not().isEmpty(),
+        check('password', 'La contraseña es obligatoria').not().isEmpty(),
+        check('email', 'El email es obligatorio').isEmail(),
+        validarCampos
+    ], 
+    postUsuarios );
 
 
 module.exports = router;
