e85318781c7e70ac8ab4ce717d0849b6b02448de
HectorRoman <hectorr9577@gmail.com>
2022-12-24 10:08:25 -0600

    Subir archivos al servidor parte 2

--- controllers/upload.js
+++ controllers/upload.js
@@ -1,4 +1,5 @@
 const { response } = require('express');
+const { v4: uuidv4 } = require('uuid');
 
 const Usuario = require('../models/usuario');
 const Medico = require('../models/medico');
@@ -28,13 +29,46 @@ const fileUpload = async(req, res = response) =>{
         });
       }
 
-      //Procesar la imagen.....
+    //Procesar la imagen.....
+    const file = req.files.image;
+
+    const nombreCortado = file.name.split('.');
+    const extencionArchivo = nombreCortado[ nombreCortado.length - 1 ];
+
+    //Validar Extenciones
+    const extencionesValidas = [ 'png', 'jpg', 'jpge', 'gif' ];
+
+    if( !extencionesValidas.includes( extencionArchivo ) ){
+        return res.status(400).json({
+            ok: false,
+            msg: 'tipo de archivo no valido'
+        })
+    }
+
+    //Generar el nombre del archivo
+    const nombreArchivo = `${ uuidv4() }.${ extencionArchivo }`;
+
+    //Path para guardar la imagen
+    const path = `./uploads/${ tipo }/${ nombreArchivo }`;
+
+    // Mover la imagen
+    file.mv( path, (err) => {
+      if (err){
+          return res.status(500).json({
+            ok: false,
+            msg: 'Error Inesperado al mover la imagen'
+        })
+      }
+
+      res.json({
+          ok: true,
+          msg: 'Archivo Subido',
+          nombreArchivo
+      })
+      
+    });
 
 
-    res.json({
-        ok: true,
-        msg: 'file'
-    })
 }
 
 
